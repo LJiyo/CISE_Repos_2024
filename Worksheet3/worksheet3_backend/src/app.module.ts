f449995324d34cb8249fc90d0946016709da2317
Lars Bosales <116531310+LJiyo@users.noreply.github.com>
2024-08-19 20:01:23 +1200

    Refactor: Delete extra database URI as .env already has it

--- Worksheet3/worksheet3_backend/src/app.module.ts
+++ Worksheet3/worksheet3_backend/src/app.module.ts
@@ -5,10 +5,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BookModule } from './api/books/book.module';
 import { ConfigModule } from '@nestjs/config';
 
-
-const DB_URI = 
-'mongodb+srv://larsjbosales:Kitkat123@cluster0.xxt9o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 @Module({
   imports: [
     ConfigModule.forRoot(), 
