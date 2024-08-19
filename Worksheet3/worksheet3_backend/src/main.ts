bd760b2bd6787e5f08f8d83146dea122e621b02f
Lars Bosales <116531310+LJiyo@users.noreply.github.com>
2024-08-19 16:19:21 +1200

    Feat(Components): Create frontend book components

--- Worksheet3/worksheet3_backend/src/main.ts
+++ Worksheet3/worksheet3_backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8082;
-  await app.listen(port, () => console.log('Server running on port ${',port,'}'));
+  await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
